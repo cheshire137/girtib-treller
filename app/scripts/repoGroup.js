4c2cfc693b0642e122861c6ef1ce5bbb5c32c8c8
Sarah Vessels <cheshire137@gmail.com>
2015-06-03 19:34:30 -0400

    Toggle group to toggle children

--- app/scripts/repoGroup.js
+++ app/scripts/repoGroup.js
@@ -1,16 +1,23 @@
 var RepoListItem = require('./repoListItem').RepoListItem;
 exports.RepoGroup = React.createClass({
+  getInitialState: function() {
+    return {checked: false};
+  },
+  handleChange: function(event) {
+    this.setState({checked: !this.state.checked});
+  },
   render: function() {
     var listItems = [];
     for (var i=0; i<this.props.repos.length; i++) {
       var repo = this.props.repos[i];
       var key = this.props.index + '-' + i;
-      listItems.push(<RepoListItem key={key} index={i} orgIndex={this.props.index} repo={repo} />)
+      var listItem = <RepoListItem key={key} index={i} orgIndex={this.props.index} repo={repo} checked={this.state.checked} />;
+      listItems.push(listItem)
     }
     var checkboxId = 'org-' + this.props.index;
     return (
       <li className="org-list-item">
-        <input type="checkbox" className="filled-in" id={checkboxId} />
+        <input type="checkbox" checked={this.state.checked} className="filled-in" id={checkboxId} onChange={this.handleChange} />
         <label htmlFor={checkboxId} className="org-name">
           {this.props.orgName}
         </label>
--- app/scripts/repoListItem.js
+++ app/scripts/repoListItem.js
@@ -1,4 +1,13 @@
 exports.RepoListItem = React.createClass({
+  getInitialState: function() {
+    return {checked: this.props.checked};
+  },
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({checked: nextProps.checked});
+  },
+  handleChange: function(event) {
+    this.setState({checked: !this.state.checked});
+  },
   render: function() {
     var icon = this.props.repo.private ? (
       <span data-tooltip="Private" data-position="right" className="tooltipped private-indicator mdi-action-lock"></span>
@@ -6,14 +15,14 @@ exports.RepoListItem = React.createClass({
     var checkboxId = 'repo-' + this.props.orgIndex + '-' + this.props.index;
     return (
       <li className="repo-list-item">
-        <input type="checkbox" className="filled-in" id={checkboxId} />
+        <input type="checkbox" checked={this.state.checked} className="filled-in" id={checkboxId} onChange={this.handleChange} />
         <label htmlFor={checkboxId}>
           {this.props.repo.name}
         </label>
+        {icon}
         <a className="repo-link" href={this.props.repo.html_url} target="_blank">
           <i className="mdi-action-open-in-new"></i>
         </a>
-        {icon}
       </li>
     );
   }
