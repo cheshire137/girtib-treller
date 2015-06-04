b276cddc150fa7b5e4e327e3ea2c388ac458ec9e
Sarah Vessels <cheshire137@gmail.com>
2015-06-03 20:37:35 -0400

    Select the right month
    Fixes #3

--- app/scripts/commitsList.js
+++ app/scripts/commitsList.js
@@ -3,12 +3,20 @@ exports.CommitsList = React.createClass({
   getMonthOptions: function() {
     var monthsToShow = 3;
     var options = [];
+    var curDay = new Date().getDay();
+    var monthIndex;
+    if (curDay >= 15) {
+      monthIndex = 0;
+    } else {
+      monthIndex = 1;
+    }
     for (var i=0; i<=3; i++) {
       var date = moment().subtract(i, 'months');
       var label = date.format('MMMM YYYY');
       var value = date.format('YYYY-MM');
+      var selected = i === monthIndex ? 'selected' : '';
       var option = (
-        <option value={value}>{label}</option>
+        <option value={value} selected={selected}>{label}</option>
       );
       options.push(option);
     }
